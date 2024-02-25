15f5ef06d58ba9b317d7fdfb664c5c59db70ba94
moralyda817 <160478147+moralyda817@users.noreply.github.com>
2024-02-25 16:49:59 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const sudo = require('sudo-prompt');
-const options = {
-  name: 'pargar',
-};
+const express = require('express');
+const http = require('http');
 
-const command = 'sudo sysctl net.ipv4.ip_forward=1';
+const app = express();
 
-sudo.exec(command, options, function(error, stdout, stderr) {
-  if (error) throw error;
-  console.log('stdout: ' + stdout);
+// تنظیم پورت و آدرس آی‌پی مقصد
+const destinationIP = '64.227.32.106';
+const destinationPort = 80;
+
+app.use(express.json());
+
+app.all('*', (req, res) => {
+  const options = {
+    hostname: destinationIP,
+    port: destinationPort,
+    path: req.path,
+    method: req.method,
+    headers: req.headers,
+  };
+
+  const proxyReq = http.request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res, {
+      end: true
+    });
+  });
+
+  req.pipe(proxyReq, {
+    end: true
+  });
+
+  proxyReq.on('error', (err) => {
+    console.error('Proxy request failed:', err);
+    res.status(500).send('Proxy request failed');
+  });
+});
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
